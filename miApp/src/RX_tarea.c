d4746c212f101634d7e0770232d1b90ad0191b8e
Santiago Esteva <santiagoesteva@hotmail.com>
2020-12-09 19:13:23 -0300

    modifico los mensajes de UART

--- miApp/src/RX_tarea.c
+++ miApp/src/RX_tarea.c
@@ -16,21 +16,21 @@ void onRx( void *noUsado )
 	//define el estado dependiendo de lo que recibe por UART
 	if(ESTADO1 == maquina){
 		//printf( "Se recibe parametro correcto, estado de MEF seleccionado: NORMAL\r\n");
-		uartWriteString( UART_USB,"Se recibe parametro correcto, estado de MEF seleccionado: NORMAL\r\n");
+		uartWriteString( UART_USB,"Se recibe parametro correcto, MEF seleccionada: NORMAL\r\n");
 		*parametro = 0;
 	}else if (ESTADO2 == maquina){
 		//printf( "Se recibe parametro correcto, estado de MEF seleccionado: DESCONECTADO\r\n");
-		uartWriteString( UART_USB, "Se recibe parametro correcto, estado de MEF seleccionado: DESCONECTADO\r\n");
+		uartWriteString( UART_USB, "Se recibe parametro correcto, MEF seleccionada: DESCONECTADO\r\n");
 		*(parametro+1) = 0;
 	}else if (ESTADO3 == maquina){
 		//printf( "Se recibe parametro correcto, estado de MEF seleccionado: ALARMA\r\n");
-		uartWriteString( UART_USB,"Se recibe parametro correcto, estado de MEF seleccionado: ALARMA\r\n");
+		uartWriteString( UART_USB,"Se recibe parametro correcto, MEF seleccionada: ALARMA\r\n");
 		*(parametro+2) = 0;
 	}else {
 		// se informa el dato erroneo por uart
 		//printf( "Dato enviado incorrecto, los estados posibles son:\r\n");
 		//printf( "1 -> NORMAL   2 -> DESCONECTADO   3 -> ALARMA  \r\n");
-		uartWriteString( UART_USB,"Dato enviado incorrecto, los estados posibles son:\r\n");
+		uartWriteString( UART_USB,"Dato enviado incorrecto, las MEF posibles son:\r\n");
 		uartWriteString( UART_USB,"1 -> NORMAL   2 -> DESCONECTADO   3 -> ALARMA  \r\n");
 	}
 
@@ -61,13 +61,13 @@ void RX_Update(void *estado)
 	//Define el mensaje dependiendo el estado
 	if(!*estado_actual){
 		//printf( "La MEF se encuentra en el estado NORMAL\r\n");
-		uartWriteString( UART_USB,"La MEF se encuentra en el estado NORMAL\r\n");
+		uartWriteString( UART_USB,"La MEF actual es la NORMAL\r\n");
 	}else if (!*(estado_actual+1)){
 		//printf( "La MEF se encuentra en el estado DESCONECTADO\r\n");
-		uartWriteString( UART_USB,"La MEF se encuentra en el estado DESCONECTADO\r\n");
+		uartWriteString( UART_USB,"La MEF actual es la DESCONECTADO\r\n");
 	}else if (!*(estado_actual+2)){
 		//printf( "La MEF se encuentra en el estado ALARMA\r\n");
-		uartWriteString( UART_USB,"La MEF se encuentra en el estado ALARMA\r\n");
+		uartWriteString( UART_USB,"La MEF actual es la ALARMA\r\n");
 	}else{
 		//printf( "Estado erroneo, enviar un nuevo estado con la codificacion correcta\r\n");
 		uartWriteString( UART_USB,"Estado erroneo, enviar un nuevo estado con la codificacion correcta\r\n");
@@ -75,9 +75,9 @@ void RX_Update(void *estado)
 
 	if (!*(parametro+3)){
 		//printf( "La MEF seleccionada es la LED\r\n");
-		uartWriteString( UART_USB,"La MEF seleccionada es la LED\r\n");
+		uartWriteString( UART_USB,"La MEF seleccionada contiene es estado LED\r\n");
 	}else{
 		//printf( "La MEF seleccionada es la RGB\r\n");
-		uartWriteString( UART_USB,"La MEF seleccionada es la RGB\r\n");
+		uartWriteString( UART_USB,"La MEF seleccionada contiene es estado RGB\r\n");
 	}
 }
